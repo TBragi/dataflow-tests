7cc785f8b890fd19402f5cadf7fe02b54669a8bb
Tomas Bragi Reynisson <tbr@beumergroup.com>
2022-01-13 10:31:39 +0100

    dataflow with and without schema

--- kafka-ecosystem/consumer/consumer.py
+++ kafka-ecosystem/consumer/consumer.py
@@ -4,14 +4,18 @@ from json import loads
 
 time.sleep(10)
 
-consumer = KafkaConsumer(
-    'numtest',
-     bootstrap_servers=['kafka:9094'],
+schemaRegistry = 'http://schema-registry:8081'
+kafkaBroker = ['kafka:9094']
+topicName = 'data'
+
+simple_consumer = KafkaConsumer(
+    topicName,
+     bootstrap_servers=kafkaBroker,
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 
-for message in consumer:
+for message in simple_consumer:
     print('key: {} \nvalue: {}'.format(str(message.key), message.value))
--- kafka-ecosystem/producer/producer.py
+++ kafka-ecosystem/producer/producer.py
@@ -1,15 +1,53 @@
 import time
 from json import dumps
 from kafka import KafkaProducer
+from kafka_schema_registry import prepare_producer
 
 time.sleep(10)
-producer = KafkaProducer(bootstrap_servers=['kafka:9094'],
+
+
+SAMPLE_SCHEMA = {
+    "type": "record",
+    "name": "TestType",
+    "fields" : [
+        {"name": "age", "type": "int"},
+        {"name": "name", "type": ["null", "string"]}
+    ]
+}
+schemaRegistry = 'http://schema-registry:8081'
+kafkaBroker = ['kafka:9094']
+topicName = 'data'
+
+simple_producer = KafkaProducer(bootstrap_servers=kafkaBroker,
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
+
+
+schema_producer = prepare_producer(
+        kafkaBroker,
+        f'{schemaRegistry}',
+        topicName +'-schema',
+        1,
+        1,
+        value_schema=SAMPLE_SCHEMA,
+)
+
+exist_schema_producer = prepare_producer(
+        kafkaBroker,
+        f'{schemaRegistry}',
+        topicName +'-schema',
+        1,
+        1,
+)
 
 
 for e in range(1000):
     data = {'number' : e}
     keys = b'yes'
-    producer.send('numtest', key=keys, value=data)
+    simple_producer.send(topicName, key=keys, value=data)
+    schema_producer.send(topicName +'-schema', {'age': e})
+    schema_producer.send(topicName +'-schema', {'age': e+32, 'name': 'john'})
+    
+    exist_schema_producer.send(topicName +'-schema', {'age': e+32, 'name': 'dump'})
+    # simple_producer.send(topicName + '-schema', value='this is a bad schema writer')
     time.sleep(5)
